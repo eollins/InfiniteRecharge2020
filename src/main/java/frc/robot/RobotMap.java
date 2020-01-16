a1ca600e854ebcfe6f1ec8f5419693f90f8d2227
Ethan Ollins <ethanollins6@gmail.com>
2020-01-15 17:56:41 -0800

    Wrote initial port mappings and created DriveTrain subsystem - still need to fully initialize the speed controllers\

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,26 +1,22 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
-public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+public class RobotMap {
+  //Speed controller port mappings
+  public static final int frontLeftMotor = 1;
+  public static final int frontRightMotor = 2;
+  public static final int backLeftMotor = 3;
+  public static final int backRightMotor = 4;
+  
+  //Joystick port mappings
+  public static final int primaryJoystick = 5;
+  public static final int secondaryJoystick = 6;
+
+  public static SpeedController frontLeft;
+  public static SpeedController frontRight;
+  public static SpeedController backLeft;
+  public static SpeedController backRight;
 }
