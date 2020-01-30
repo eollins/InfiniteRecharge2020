195d1fa250963e2c697e54b6662ac66c5ad3b297
Ethan Ollins <ethanollins6@gmail.com>
2020-01-30 14:11:37 -0800

    Updated to 2020 SDK and firmware

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,21 +7,18 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.robot.RobotMap;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-//Includes functions for both arcade drive and tank drive control.
-public class DriveTrain extends Subsystem {
+public class DriveTrain extends SubsystemBase {
+  /**
+   * Creates a new DriveTrain.
+   */
+  public DriveTrain() {
+
+  }
+
   @Override
-  public void initDefaultCommand() {
-    
-  }
-
-  public static void arcadeDrive(double x, double y) {
-    RobotMap.robotDrive.arcadeDrive(x, y);
-  }
-
-  public static void tankDrive(double x, double y) {
-    RobotMap.robotDrive.tankDrive(x, y);
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
