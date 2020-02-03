cafb1daafdbbcf213a62b4cae0f6ccdb18edadfc
DESKTOP-H33JJKE\EthanOllins <ethanollins6@gmail.com>
2020-02-03 14:30:56 -0800

    Added new command and subsystem for intake motor

---
+++ src/main/java/frc/robot/IntakeMotor.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeMotor extends SubsystemBase {
+  /**
+   * Creates a new IntakeMotor.
+   */
+  public IntakeMotor() {
+
+  }
+
+  public static void SetMotor(boolean direction) {
+    int multiplier = 1;
+    if (direction == false) {
+      multiplier = -1;
+    }
+    Constants.intakeMotor.set(Constants.intakePower * multiplier);
+  }
+
+  public static void StopMotor() {
+    Constants.intakeMotor.set(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
