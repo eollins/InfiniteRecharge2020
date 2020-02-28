c8420e17f2e20b6c2096cebf64949565f342c130
Ethan Ollins <ethanollins6@gmail.com>
2020-02-28 12:12:47 -0800

    Began experimenting with Pathweaver

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,17 +7,26 @@
 
 package frc.robot;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -45,16 +54,17 @@ public class Robot extends TimedRobot {
   private Encoder encoder;
 
   /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
 
-    //Instantiate all motors
+    // Instantiate all motors
     frontLeft = new VictorSPX(Constants.frontLeftMotor);
     Constants.frontLeft = frontLeft;
     backLeft = new VictorSPX(Constants.backLeftMotor);
@@ -68,7 +78,7 @@ public class Robot extends TimedRobot {
     intakeMotor = new VictorSP(Constants.intakeMotorPort);
     Constants.intakeMotor = intakeMotor;
 
-    //Instantiate joysticks/controllers
+    // Instantiate joysticks/controllers
     primaryJoystick = new Joystick(Constants.primaryJoystick);
     Constants.joystickPrimary = primaryJoystick;
     secondaryJoystick = new Joystick(Constants.secondaryJoystick);
@@ -76,7 +86,7 @@ public class Robot extends TimedRobot {
     xBoxController = new XboxController(Constants.xBoxControllerPort);
     Constants.xBoxController = xBoxController;
 
-    //Instantiate buttons
+    // Instantiate buttons
     JoystickButton intakeToggleForward = new JoystickButton(xBoxController, Constants.intakeForward);
     JoystickButton intakeToggleBackward = new JoystickButton(xBoxController, Constants.intakeBackward);
     JoystickButton switchDriveMode = new JoystickButton(primaryJoystick, Constants.tankToArcade);
@@ -100,17 +110,22 @@ public class Robot extends TimedRobot {
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
@@ -127,16 +142,20 @@ public class Robot extends TimedRobot {
   }
 
   /**
-   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
    */
   @Override
   public void autonomousInit() {
-    //m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    // if (m_autonomousCommand != null) {
-    //   m_autonomousCommand.schedule();
-    // }
+    // Import autonomous paths
+    String path = "paths/TestPath.path";
+    Path importedPath = Filesystem.getDeployDirectory().toPath().resolve(path);
+    try {
+      Trajectory trajectory = TrajectoryUtil.fromPathweaverJson(importedPath);
+      DifferentialDrive drive = new DifferentialDrive(new SpeedControllerGroup((SpeedController)frontLeft, (SpeedController)frontRight), new SpeedControllerGroup((SpeedController)frontRight, (SpeedController)backRight));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   /**
